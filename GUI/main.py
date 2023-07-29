184cb032214e15ca49545b141e89c3bbf552185b
krrish <87867238+krrish-v@users.noreply.github.com>
2023-07-30 02:31:25 +0530

    adding more feature

--- GUI/main.py
+++ GUI/main.py
@@ -7,7 +7,10 @@ from kivy.clock import Clock
 
 import speech_recognition as sr
 import time
+import pickle
 from elevenlabs import generate, play, set_api_key
+
+from books_recommend import get_pdf
 
 set_api_key("<API KEY>")
 
@@ -18,9 +21,7 @@ from langchain.memory import ConversationBufferWindowMemory
 # set memory buffer window to 1 to avoid passing excessive tokens-->
 memory = ConversationBufferWindowMemory(k=1) # stores previous k conversations between Ai and human
 
-
-
-llm = ChatOpenAI(temperature=0.0,openai_api_key="<API KEY>")
+llm = ChatOpenAI(temperature=0.0,openai_api_key="API KEY")
 memory = ConversationBufferWindowMemory(k=1)
 conversation = ConversationChain(
     llm=llm,
@@ -28,6 +29,19 @@ conversation = ConversationChain(
     verbose=False
 )
 
+f= open('model1.hy', 'rb')
+nlp = pickle.load(f)
+f.close()
+
+def extract_entities(text):
+    doc = nlp(text)
+    
+    entities = ''
+    for ent in doc.ents:
+        if ent.label_ in ["NORP", "ORG", "GPE", "PERSON", "BOOK", "WORK_OF_ART"]:  # Filter relevant entities like nationalities, organizations, geopolitical entities, and person names
+            entities+=ent.text +' '
+    
+    return entities
 
 # Define the KivyMD KV language string
 KV = """
@@ -60,6 +74,7 @@ KV = """
 
 # Create the main screen class
 class MainScreen(BoxLayout):
+    count = 0
     
     def engine(self, dt):
         self.r = sr.Recognizer()
@@ -70,47 +85,87 @@ class MainScreen(BoxLayout):
             self.speaker('Speak now')
             self.r.adjust_for_ambient_noise(source,duration=1)
             self.audio= self.r.listen(source, timeout=0)
-            
+
             Clock.schedule_once(self.on_voice, 0.1)
 
-            
+
     def recog(self, dts):
-        self.text = self.r.recognize_google(self.audio) # google is used a recognisor
-        Clock.schedule_once(lambda x: self.display(self.text), 0.1)
-
-    # Define the callback for the voice icon press
-    def on_voice(self, dt):
-        
-        self.speaker('Please wait for the response')
-        self.ids.voice.md_bg_color = 'red'
-
         try:
-            Clock.schedule_once(self.recog, 0.1)
+            self.text = self.r.recognize_google(self.audio) # google is used a recognisor
+
+            if self.count == 0:
+                Clock.schedule_once(lambda x: self.display(self.text), 0.1)
+            else:
+                Clock.schedule_once(lambda x: self.read_doc(self.text), 0.1)
+
         except:
             self.speaker('Unable to recognise your voice Try again')
             time.sleep(1)
             Clock.schedule_once(self.engine, 0.1)
+            
+
+    # Define the callback for the voice icon press
+    def on_voice(self, dt):
+        self.speaker('Please wait for the response')
+        self.ids.voice.md_bg_color = 'red'
+
+        Clock.schedule_once(self.recog, 0.1)
 
     def display(self, sent):
-
-        # Rest of the code
-        #-----------------------
+        
         if 'exit' in sent:
             self.speaker('Exiting the program')
             exit()
+            
+        elif 'download' in sent and 'book' in sent:
+
+            subjects = extract_entities(sent)
+            if subjects != '':
+                self.speaker(f'Downloading a book {subjects} form internet')
+                self.doc_text = get_pdf(subjects)
+
+                if self.doc_text is not None:
+                    Clock.schedule_once(lambda x: self.read_doc(text=''), 0.1)
+                else:
+                    self.speaker(f'Unable to find the book {subjects} you asked for, try for some another book')
+                    self.ids.voice.md_bg_color = 'green'
+                    Clock.schedule_once(self.engine, 0.1)
+            else: 
+                self.speaker(f'Unable to get the name of a book you asked')
+                self.ids.voice.md_bg_color = 'green'
+                Clock.schedule_once(self.engine, 0.1)
+
         else:
             output = conversation.predict(input=sent)
+
             self.speaker(output)
-        #-----------------------
+            self.ids.voice.md_bg_color = 'green'            
+            Clock.schedule_once(self.engine, 0.1)
 
-        self.ids.voice.md_bg_color = 'green'
 
-        Clock.schedule_once(self.engine, 0.1)
+    def read_doc(self, text):
+        
+        try:
+            if 'stop' in text:
+                self.count = 0
+                self.speaker('Stoped reading file')
+                self.ids.voice.md_bg_color = 'green'
+                Clock.schedule_once(self.engine, 0.1)
+            
+            else:
+                self.speaker(f'Starting the phase {self.count}    ' +self.doc_text[self.count])
+                self.speaker(self.doc_text[self.count])
+                self.count+=1
+                self.ids.voice.md_bg_color = 'green'
+                Clock.schedule_once(self.engine, 0.1)
+        except:
+            self.speaker('End of the file')
+
 
     def speaker(self, sent):
         audio = generate(
             text=sent,
-            voice="Bella",
+            voice="Daniel",
             model="eleven_monolingual_v1"
         )
 
@@ -118,7 +173,7 @@ class MainScreen(BoxLayout):
 
     def start(self, dt):
         time.sleep(1)
-        self.speaker('Starting up the program')
+        #self.speaker('Starting up the program')
         Clock.schedule_once(self.engine, 0.1)
 
 # Create the main app class
