08bd3eb61bccea763d779c744b0c85e82a4be725
krrish <87867238+krrish-v@users.noreply.github.com>
2023-07-30 19:11:47 +0530

    Add files via upload

---
+++ GUI/weather.py
@@ -0,0 +1,19 @@
+import requests
+
+def get_weather(city):
+    base_url = "http://api.openweathermap.org/data/2.5/weather"
+    params = {"q": city, "appid": "1951d74efc0db26fdeb2bb999b5977f0", "units": "metric"}
+
+    response = requests.get(base_url, params=params)
+    data = response.json()
+
+    if data["cod"] == 200:
+        weather_description = data["weather"][0]["description"]
+        temperature = data["main"]["temp"]
+        humidity = data["main"]["humidity"]
+        wind_speed = data["wind"]["speed"]
+
+        forecast = f"Weather forecast for {city}: {weather_description}, Temperature: {temperature}°C, Humidity: {humidity}%, Wind Speed: {wind_speed} m/s"
+        return forecast
+    else:
+        return "Unabel to fetch the weather of your city"
